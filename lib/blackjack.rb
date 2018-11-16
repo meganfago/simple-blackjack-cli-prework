cd921f7317a6e6273ec65989a96cd3e7d7bef95c
Megan Fago <meganlaynefago@gmail.com>
2018-11-16 19:58:50 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,7 +29,14 @@ def initial_round
   return sum
 end
 
-def hit?
+def hit?(current_total)
+  prompt_user
+  input = get_user_input
+  if input == 'h'
+    return current_total += deal_card
+  elsif input == 's'
+    return current_total
+  end 
 
 end
 
